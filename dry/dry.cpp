4b9ca8a5905af8905e08b3547940b35f213916fa
Omar Marana <omarmarana@campus.technion.ac.il>
2022-01-07 20:38:05 +0200

    Everything ready, waiting on updated biadsy tests

--- dry/dry.cpp
+++ dry/dry.cpp
@@ -34,9 +34,6 @@ std::vector<T> slice(std::vector<T> vec, int start, int step, int stop)
 }
 
 
-// template<class T>
-// std::vector<T> slice(std::vector<T> vec, int start, int step, int stop);
-
 class A {
     public:
         std::vector<std::shared_ptr<int>> values;
